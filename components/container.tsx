737ca6edca44a9b60f267321beb801e92204d5c1
andrianfaa <andrianfadhilla@gmail.com>
2022-04-06 14:36:02 +0700

    fix build error

--- components/container.tsx
+++ components/container.tsx
@@ -4,23 +4,19 @@ import type { ReactNode, ReactElement } from 'react';
 
 export interface ContainerProps {
   children: ReactNode;
-  maxWidth?: number;
   className?: string;
   id?: string;
 }
 
 export default function Container({
   children,
-  maxWidth,
   className,
   id,
 }: ContainerProps): ReactElement {
   return (
     <div
       id={id}
-      className={`w-full ${
-        maxWidth ? `max-w-[${maxWidth}px]` : 'max-w-[900px]'
-      } mx-auto p-6 ${className}`}
+      className={`w-full max-w-[900px] mx-auto p-6 ${className}`}
     >
       {children}
     </div>
