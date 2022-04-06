568471bfc64acfb5e8fb61b737b891c5643d2857
andrianfaa <andrianfadhilla@gmail.com>
2022-04-07 01:53:33 +0700

    update 07-04-22 - update baseUrl for production

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 export default {
-  baseUrl: (path: string = '') => `http://localhost:3000${path}`, // Development
-  // baseUrl: (path: string = '') => `https://andriann.co${path}`, // Production
+  // baseUrl: (path: string = '') => `http://localhost:3000${path}`, // Development
+  baseUrl: (path: string = '') => `https://andriann.co${path}`, // Production
 };
