90411f99c32a74dffe8a3e81237be2985d95e171
andrianfaa <andrianfadhilla@gmail.com>
2022-04-07 16:40:46 +0700

    feat: added favicons and manifest.json

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,14 @@ export default function Document(): React.ReactElement {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=JetBrains+Mono:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png?v=1" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png?v=1" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png?v=1" />
+        <link rel="manifest" href="/manifest.json?v=1" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg?v=1" color="#0f172a" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico?v=1" />
+        <meta name="msapplication-TileColor" content="#0f172a" />
+        <meta name="msapplication-config" content="/favicons/browserconfig.xml?v=1" />
         <meta name="theme-color" content="#0f172a" />
       </Head>
       <body>
