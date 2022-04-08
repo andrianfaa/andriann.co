335c8ea75f36c54fd35461e9679fa610d16650d2
andrianfaa <andrianfadhilla@gmail.com>
2022-04-08 20:26:11 +0700

    fix(pages/api): fix api can't sort on portfolio page

--- app/config.ts
+++ app/config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default {
   // baseUrl: (path: string = '') => `http://localhost:3000${path}`, // Development
   baseUrl: (path: string = '') => `https://andriann.co${path}`, // Production
